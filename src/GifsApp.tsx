20662eb4e0ed57d96bdd62e6288eee60d2d312d4
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-09-20 00:43:46 -0500

    feat(gifs): render fetched gifs on screen

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react"
 import { GifList } from "./gifs/components/GifList"
 import { PreviousSearches } from "./gifs/components/PreviousSearches"
-import { mockGifs } from "./mock-data/gifs.mock"
 import { CustomHeader } from "./shared/components/CustomHeader"
 import { SearchBar } from "./shared/components/SearchBar"
 import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action"
+import type { Gif } from "./gifs/interfaces/gif.interface"
 
 export const GifsApp = () => {
-    const [previousTerms, setPreviousTerms] = useState(['dragon ball z']);
+    const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+    const [gifs, setGifs] = useState<Gif[]>([]);
 
     const handleTermClicked = (term: string) => {
         console.log({ term });
@@ -16,15 +17,14 @@ export const GifsApp = () => {
     const handleSearch = async (query: string = "") => {
         query = query.trim().toLowerCase();
 
-        if(query.length === 0) return
-        
-        if(previousTerms.includes(query)) return;
+        if (query.length === 0) return
+
+        if (previousTerms.includes(query)) return;
 
         setPreviousTerms([query, ...previousTerms].splice(0, 8));
 
         const gifs = await getGifsByQuery(query);
-
-        console.log({gifs});
+        setGifs(gifs);
 
     }
 
@@ -42,7 +42,7 @@ export const GifsApp = () => {
 
 
             {/* Gifs */}
-            <GifList gifs={mockGifs} />
+            <GifList gifs={gifs} />
         </>
     )
 }
